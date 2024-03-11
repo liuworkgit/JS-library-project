1acf546c605dfd73be411881732ba0f6f0fdbd7f
moses21 <moses21@student.ubc.ca>
2024-03-10 17:49:05 -0700

    added classes to book entry fields for js functionality, worked on displayBooks function

--- libraryfunctionality.js
+++ libraryfunctionality.js
@@ -40,4 +40,38 @@ function addBookToLibrary() {
     library.push(inputToObject());
 }
 
+/**
+ * loops through the testLibrary array and displays
+ * its contents on the webpage
+ */
+function displayBooks() {
+    if (testLibrary != []) {
+        for (let book in testLibrary) {
+            let newEntry = document.createElement("div");
+            newEntry.className = "book-entry";
+
+            newEntry.getElementsByClassName("book-title") = book.title;
+            newEntry.getElementsByClassName("book-author") = book.author;
+            newEntry.getElementsByClassName("book-num-pages") = "Number of pages:" 
+            + (book.pageNum).toString();
+
+            document.getElementById("book-display").appendChild(newEntry);
+        }
+    }
+}
+
+// temporary library for testing purposes
+let testLibrary = [];
+book1 = new Book("A Court of Thorns and Roses", "Sarah J. Maas", 300);
+book2 = new Book("Dogman Unleashed", "Dav Pilkey", 150);
+testLibrary.push(book1);
+testLibrary.push(book2);
+
+// TEST BUTTON
+let button = document.getElementById("testbutton");
+button.addEventListener("click", function () {
+    displayBooks();
+    console.log("button pressed!");
+}, false);
+
 module.exports = { addBookToLibrary }
